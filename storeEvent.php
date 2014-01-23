820a13c24a9ba47938dffb78ebf89e9977b7f695
shage001 <shage001@gmail.com>
2014-01-22 23:30:00 -0500

    I wish github worked better

--- storeEvent.php
+++ storeEvent.php
@@ -19,16 +19,16 @@ $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("
 $eid = rand(10000000, 99999999);
 
 $sql = "INSERT INTO Events (EID, Name, Date, StartTime, EndTime, Location, Organization, Description) 
-
-VALUES ('$eid', '$_POST[eventName]', '$_POST[eventDate]', '$_POST[startEventTime]', '$_POST[endEventTime]',
-'$_POST[eventLocation]', '$_POST[eventOrganization]', '$_POST[eventDescription]')";
+VALUES ('$eid', '$_POST[eventName]', '$_POST[eventDate]', '$_POST[startEventTime]', '$_POST[endEventTime]', '$_POST[eventLocation]', '$_POST[eventOrganization]', '$_POST[eventDescription]')";
 
 //$sql_2 = "INSERT INTO Tags (Tag_Name, EID)
 //VALUES ('$_POST[tag]', $_POST[EID]')"; 
 
+
 echo "Thank you!<br><br>Name: $_POST[eventName]<br>Event ID: $eid<br>Location: $_POST[eventLocation]
 <br>Organization: $_POST[eventOrganization]<br>Date: $_POST[eventDate]<br>Start time: $_POST[startEventTime]
-<br>End time: $_POST[endEventTime";
+<br>End time: $_POST[endEventTime]";
+
 //echo $sql_2; 
 
 echo "New entry added"
