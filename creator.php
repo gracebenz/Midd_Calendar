e7f5da6068097fe40d122f9c3b79519001595109
argentconflagration <atcg526@aol.com>
2014-01-27 20:56:47 -0500

    creator to creator

--- creator.php
+++ creator.php
@@ -56,7 +56,7 @@ $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("
 
 <html>
 	<head>
-		<!--<link type="text/css" rel="stylesheet" href="entryStylesheet.css"/>-->
+		<link type="text/css" rel="stylesheet" href="entryStylesheet.css"/>
 		<title>User Registration</title>
 	</head>
 
@@ -95,6 +95,7 @@ if(isset($_POST['register_submit']))
 		$username = $con->real_escape_string($_POST['Username']);
 		$password = $con->real_escape_string($_POST['Password']);
 		$encrpass = encrypt_decrypt('encrypt', $password);
+		//$verifyid = rand(10000000, 99999999);
 		
 		$sql = "INSERT INTO Creators (Username, Password) 
 		VALUES ('$username', '$encrpass')";
@@ -104,8 +105,8 @@ if(isset($_POST['register_submit']))
 			}
 		
 		$to = $username."@middlebury.edu";
-		$subject = "This is subject";
-		$message = "This is simple text message.";
+		$subject = "Middlebury Events Calendar Account Confirmation";
+		$message = "This email is to confirm ";
 		$header = "From:noreply@noreply.org \r\n";
 
 		$retval = mail($to, $subject, $message, $header);
